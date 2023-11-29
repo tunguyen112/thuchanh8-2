c8f702646a462688de6fb5c969c92afd3010a514
tunguyen112 <152349495+tunguyen112@users.noreply.github.com>
2023-11-29 10:48:34 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class_list = {'0': 'Normal','1': 'Pneumonia'}
 st.title('Pneumonia Detecion')
 
 input = open('lrc_xray.pkl', 'rb')
-model = plk.load(input)
+model = pkl.load(input)
 
 st.header('Upload an image')
 image = st.file_uploader('Choose an image', type=(['png', 'jpg', 'jpeg']))
@@ -16,3 +16,7 @@ image = st.file_uploader('Choose an image', type=(['png', 'jpg', 'jpeg']))
 if image is not None:
   image = Image.open(image)
   st.image(image, caption='Test image')
+
+  if st.button('Predict'):
+    image = image.resize((227*227*3, 1))
+    vector = 

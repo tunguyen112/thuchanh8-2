06c4e3e2c8b743515690c32aa01748b0ffa33a68
tunguyen112 <152349495+tunguyen112@users.noreply.github.com>
2023-11-29 10:38:21 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import streamlit as st
 from PIL import Image
 import pickle as pkl
 import numpy
+
+class_list = {'0': 'Normal','1': 'Pneumonia'}
+
+st.title('Pneumonia Detecion')
+
+input = open('lrc_xray.pkl', 'rb')
+model = plk.load(input)
+
+st.header('Upload an image')
+image = st.file_uploader('Choose an image', type=(['png', 'jpg', 'jpeg']))
+
+if image is not None:
+  image = Image.open(image)
+  st.image(image, caption='Test image')
